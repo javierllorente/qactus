a59c2fb3529e73ee5a7ad23c295faef98c74561d
Javier Llorente <javier@opensuse.org>
2015-02-27 19:31:28 +0100

    bump version to 0.4.0

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setApplicationName("Qactus");
-    a.setApplicationVersion("0.3.6");
+    a.setApplicationVersion("0.4.0");
     MainWindow w;
     w.show();
     return a.exec();
