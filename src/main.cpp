972d30e7079c7459806f3956679b0e504a2bb1a5
Javier Llorente <javier@opensuse.org>
2016-01-02 00:01:08 +0100

    Use QACTUS_VERSION

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setApplicationName("Qactus");
-    a.setApplicationVersion("0.7.0");
+    a.setApplicationVersion(QACTUS_VERSION);
     MainWindow w;
     w.show();
     return a.exec();
