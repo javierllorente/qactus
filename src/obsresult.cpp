8cf024819831a9904280b4b6fc99f6211c3af473
Javier Llorente <javier@opensuse.org>
2016-04-28 01:41:54 +0200

    New class: OBSResult

---
+++ src/obsresult.cpp
@@ -0,0 +1,91 @@
+/*
+ *  Qactus - A Qt based OBS notifier
+ *
+ *  Copyright (C) 2016 Javier Llorente <javier@opensuse.org>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "obsresult.h"
+
+OBSResult::OBSResult()
+{
+    package = new OBSPackage();
+}
+
+OBSResult::~OBSResult()
+{
+    delete package;
+}
+
+QString OBSResult::getProject() const
+{
+    return project;
+}
+
+void OBSResult::setProject(const QString &value)
+{
+    project = value;
+}
+
+QString OBSResult::getRepository() const
+{
+    return repository;
+}
+
+void OBSResult::setRepository(const QString &value)
+{
+    repository = value;
+}
+
+QString OBSResult::getArch() const
+{
+    return arch;
+}
+
+void OBSResult::setArch(const QString &value)
+{
+    arch = value;
+}
+
+QString OBSResult::getCode() const
+{
+    return code;
+}
+
+void OBSResult::setCode(const QString &value)
+{
+    code = value;
+}
+
+QString OBSResult::getState() const
+{
+    return state;
+}
+
+void OBSResult::setState(const QString &value)
+{
+    state = value;
+}
+
+OBSPackage *OBSResult::getPackage() const
+{
+    return package;
+}
+
+void OBSResult::setPackage(OBSPackage *value)
+{
+    package = value;
+}
---
+++ src/obsresult.h
@@ -0,0 +1,60 @@
+/*
+ *  Qactus - A Qt based OBS notifier
+ *
+ *  Copyright (C) 2016 Javier Llorente <javier@opensuse.org>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef OBSRESULT_H
+#define OBSRESULT_H
+
+#include <QString>
+#include "obspackage.h"
+
+class OBSResult
+{
+public:
+    OBSResult();
+    ~OBSResult();
+
+    QString getProject() const;
+    void setProject(const QString &value);
+
+    QString getRepository() const;
+    void setRepository(const QString &value);
+
+    QString getArch() const;
+    void setArch(const QString &value);
+
+    QString getCode() const;
+    void setCode(const QString &value);
+
+    QString getState() const;
+    void setState(const QString &value);
+
+    OBSPackage *getPackage() const;
+    void setPackage(OBSPackage *value);
+
+private:
+    QString project;
+    QString repository;
+    QString arch;
+    QString code;
+    QString state;
+    OBSPackage *package;
+};
+
+#endif // OBSRESULT_H
