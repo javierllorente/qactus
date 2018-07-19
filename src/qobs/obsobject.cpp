34dd03bc94318883b66ee71a015a6b033b7eb0b9
Javier Llorente <javier@opensuse.org>
2018-07-20 00:11:40 +0200

    Add project and package fields and their getters/setters

--- src/qobs/obsobject.cpp
+++ src/qobs/obsobject.cpp
@@ -1,6 +1,46 @@
+/*
+ *  Qactus - A Qt based OBS notifier
+ *
+ *  Copyright (C) 2018 Javier Llorente <javier@opensuse.org>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include "obsobject.h"
 
 OBSObject::OBSObject()
 {
 
+}
+
+QString OBSObject::getProject() const
+{
+    return project;
+}
+
+void OBSObject::setProject(const QString &value)
+{
+    project = value;
+}
+
+QString OBSObject::getPackage() const
+{
+    return package;
+}
+
+void OBSObject::setPackage(const QString &value)
+{
+    package = value;
 }
--- src/qobs/obsobject.h
+++ src/qobs/obsobject.h
@@ -1,11 +1,42 @@
+/*
+ *  Qactus - A Qt based OBS notifier
+ *
+ *  Copyright (C) 2018 Javier Llorente <javier@opensuse.org>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef OBSOBJECT_H
 #define OBSOBJECT_H
 
+#include <QString>
 
 class OBSObject
 {
 public:
     OBSObject();
+
+    QString getProject() const;
+    void setProject(const QString &value);
+
+    QString getPackage() const;
+    void setPackage(const QString &value);
+
+private:
+    QString project;
+    QString package;
 };
 
 #endif // OBSOBJECT_H
