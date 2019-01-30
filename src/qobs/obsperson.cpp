eda20953180232dca21d9019538b2e7099069af1
Javier Llorente <javier@opensuse.org>
2019-01-30 20:35:08 +0100

    New class: OBSPerson

---
+++ src/qobs/obsperson.cpp
@@ -0,0 +1,81 @@
+/*
+ *  Qactus - A Qt-based OBS client
+ *
+ *  Copyright (C) 2019 Javier Llorente <javier@opensuse.org>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "obsperson.h"
+
+OBSPerson::OBSPerson()
+{
+
+}
+
+QString OBSPerson::getLogin() const
+{
+    return login;
+}
+
+void OBSPerson::setLogin(const QString &value)
+{
+    login = value;
+}
+
+QString OBSPerson::getEmail() const
+{
+    return email;
+}
+
+void OBSPerson::setEmail(const QString &value)
+{
+    email = value;
+}
+
+QString OBSPerson::getRealName() const
+{
+    return realName;
+}
+
+void OBSPerson::setRealName(const QString &value)
+{
+    realName = value;
+}
+
+QString OBSPerson::getState() const
+{
+    return state;
+}
+
+void OBSPerson::setState(const QString &value)
+{
+    state = value;
+}
+
+QStringList OBSPerson::getWatchList() const
+{
+    return watchList;
+}
+
+void OBSPerson::setWatchList(const QStringList &value)
+{
+    watchList = value;
+}
+
+void OBSPerson::appendWatchItem(const QString &item)
+{
+    watchList.append(item);
+}
---
+++ src/qobs/obsperson.h
@@ -0,0 +1,56 @@
+/*
+ *  Qactus - A Qt.based OBS client
+ *
+ *  Copyright (C) 2019 Javier Llorente <javier@opensuse.org>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef OBSPERSON_H
+#define OBSPERSON_H
+
+#include <QString>
+#include <QStringList>
+
+class OBSPerson
+{
+public:
+    OBSPerson();
+
+    QString getLogin() const;
+    void setLogin(const QString &value);
+
+    QString getEmail() const;
+    void setEmail(const QString &value);
+
+    QString getRealName() const;
+    void setRealName(const QString &value);
+
+    QString getState() const;
+    void setState(const QString &value);
+
+    QStringList getWatchList() const;
+    void setWatchList(const QStringList &value);
+    void appendWatchItem(const QString &item);
+
+private:
+    QString login;
+    QString email;
+    QString realName;
+    QString state;
+    QStringList watchList;
+};
+
+#endif // OBSPERSON_H
