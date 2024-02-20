73a03c82b412d6cc71e76cb24b12860286b6a7e6
Javier Llorente <javier@opensuse.org>
2024-02-20 22:46:46 +0100

    Initialise rev and time

--- src/qobs/obsrevision.cpp
+++ src/qobs/obsrevision.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2018 Javier Llorente <javier@opensuse.org>
+ * Copyright (C) 2017-2024 Javier Llorente <javier@opensuse.org>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,8 @@
 
 OBSRevision::OBSRevision()
 {
-
+    rev = 0;
+    time = 0;
 }
 
 uint OBSRevision::getRev() const
